51d6c9e201399c1f8fa6c0acf8cd65d3128f58bf
seyana <93588398+Norca0721@users.noreply.github.com>
2025-01-24 21:18:33 +0800

    fix: 莫名其妙的bug

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ async def broadcast(bot, ev: CQEvent):
     
     if "--group" not in args and "-g" not in args:
         await bot.finish(ev, "参数错误！请使用 '--group' 或 '-g' 指定群聊。\n 例如：/广播 HelloWorld --group 123456789 ")
-        return
 
     group_index = args.index("--group") if "--group" in args else args.index("-g")
     msg = " ".join(args[:group_index])
@@ -20,27 +19,32 @@ async def broadcast(bot, ev: CQEvent):
     
     try:
         if len(groups) == 1:
-            group_name = groups[0]
+            group_name = str(groups[0])
+            
+            try:
+                group_name = int(group_name)
+            except ValueError:
+                pass
+            
             # 群组 mark
             '''群组示例：
             if group_name == "测试":
                 groups = ['123456789', '987654321', '543216789']
             '''
+            
             if group_name == "测试":
                 groups = [f'{ev.group_id}']
             
-            else:
-                await bot.finish(ev, f"未知的组名：{group_name}")
+            if isinstance(group_name, str):
+                await bot.send(ev, f"未知的组名：{group_name}")
     except Exception as e:
         await bot.finish(ev, f"未知的群号：{e}")
     
     if not groups:
         await bot.finish(ev, "未指定群组 ID！")
-        return
 
     for group_id in groups:
         try:
-            # 确保 group_id 是 int 类型
             group_id = int(group_id)
             await bot.send_group_msg(group_id=group_id, message=msg)
         except ValueError:
